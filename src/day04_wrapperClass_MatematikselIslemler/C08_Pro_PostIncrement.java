bbd8cd5e14c0258ec07a57188d11d02eebf81f5b
redzhebmehmedd <recepcansf@gmail.com>
2024-06-01 10:25:42 +0200

    day04

---
+++ src/day04_wrapperClass_MatematikselIslemler/C08_Pro_PostIncrement.java
@@ -0,0 +1,28 @@
+package day04_wrapperClass_MatematikselIslemler;
+
+public class C08_Pro_PostIncrement {
+
+    public static void main(String[] args) {
+
+        int a = 20;
+        // a nin degerini olusturacaginiz bir b veriable yi atayin
+        // sonra a nin degerini 1 artirin
+
+        int b = a;
+
+        a++;
+
+        System.out.println( " a : " + a + " , b : " + b); // a : 21 , b :20
+
+        int c = 20;
+
+        int d = c ;
+
+        // c nin degerini 1 artirin,
+        // sonra c nin degerini olusturacaginiz d veriable degerini olusturun
+
+        c++;
+
+        System.out.println("c : "+ c + ",d : " + d);
+    }
+}
