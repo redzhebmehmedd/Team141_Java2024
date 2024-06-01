745b7d7c4ee7a15fec6061dfca1a16b6696b9518
redzhebmehmedd <recepcansf@gmail.com>
2024-06-01 10:24:58 +0200

    day04

---
+++ src/day04_wrapperClass_MatematikselIslemler/C07_IncrementDecrement.java
@@ -0,0 +1,42 @@
+package day04_wrapperClass_MatematikselIslemler;
+
+public class C07_IncrementDecrement {
+
+
+    public static void main(String[] args) {
+
+        int sayi =  10 ;
+
+        sayi = sayi + 5 ;
+
+        System.out.println(sayi); //15
+
+        sayi += 5 ;
+
+        System.out.println(sayi); //20
+
+        int artis = 30;
+
+        sayi += artis ; // sayi = sayi + artis ;
+
+        System.out.println(sayi); // 50
+
+        sayi = 2 ;
+
+        sayi =sayi * 3 ;
+
+        System.out.println(sayi); //6
+
+        //sayinin degeri 5 kat artiriniz
+
+        sayi *= 5;
+
+        System.out.println(sayi); //30
+
+
+
+
+
+
+    }
+}
