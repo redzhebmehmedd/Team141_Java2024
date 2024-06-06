8f9eb690338f13d1d54b20e7b0e24f98366c1111
redzhebmehmedd <recepcansf@gmail.com>
2024-06-06 13:20:02 +0200

    day06

---
+++ src/day06_ifELseStatements/C04_IfStatements.java
@@ -0,0 +1,28 @@
+package day06_ifELseStatements;
+
+import java.util.Scanner;
+
+public class C04_IfStatements {
+
+    public static void main(String[] args) {
+
+        //Kullanican bir ucgenin 3 kenar uzunlugunu alin,
+
+        // ucgen eskenar ucgen ise "Eskenar Ucgen"yazdirin
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lutfen ucgenin kenar uzunluklarini giriniz...");
+
+        double kenar1 = scanner.nextDouble();
+        double kenar2 = scanner.nextDouble();
+        double kenar3 = scanner.nextDouble();
+
+        if (kenar1 == kenar2 && kenar1 == kenar3  && kenar1>0) { // javada üclü karsilastirma olmaz
+            System.out.println("Eskenar Ucgen");
+
+
+
+
+        }
+    }
+}
