89cc7d1d5bf3b5697135dd6db984ec272494152b
redzhebmehmedd <recepcansf@gmail.com>
2024-06-06 13:05:59 +0200

    day06

---
+++ src/day06_ifELseStatements/C01_MantiksalOpreratorler.java
@@ -0,0 +1,33 @@
+package day06_ifELseStatements;
+
+public class C01_MantiksalOpreratorler {
+
+    public static void main(String[] args) {
+
+        //&& operatörü mükemmeliyetcidir
+        // tüm sartlat true ise sonuc true, sartlardan birtanesi false ise sonuc false'dir.
+
+        int a = 5;
+        int b = 3;
+        int c = 7;
+
+        System.out.println( 2*a >= b+c && 3*b != a+c && b+5 < a*c); //true
+
+
+        // OR || operatörü iyimserdir.
+        // tüm sartlar false ise sonuc false, dir.
+        // sartlardan birtanesi bile true ise sonuc true'dur.
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
